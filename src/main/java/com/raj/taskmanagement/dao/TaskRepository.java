ed0aa5622ab03fbc6217e5c1cec2531e70ef2d71
Rajdeep Banik <rbanik2212@gmail.com>
2024-10-18 13:52:26 +0100

    fixed id reset in repository

--- src/main/java/com/raj/taskmanagement/dao/TaskRepository.java
+++ src/main/java/com/raj/taskmanagement/dao/TaskRepository.java
@@ -2,6 +2,9 @@ package com.raj.taskmanagement.dao;
 
 import com.raj.taskmanagement.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Query(value = "SELECT setval(pg_get_serial_sequence('tasks', 'id'), COALESCE(MAX(id), 0)) FROM tasks", nativeQuery = true)
+    Integer resetTaskIdSequence();
 }
--- src/main/java/com/raj/taskmanagement/service/TaskServiceImpl.java
+++ src/main/java/com/raj/taskmanagement/service/TaskServiceImpl.java
@@ -2,9 +2,6 @@ package com.raj.taskmanagement.service;
 
 import com.raj.taskmanagement.dao.TaskRepository;
 import com.raj.taskmanagement.entity.Task;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,24 +15,21 @@ import java.util.Optional;
 public class TaskServiceImpl implements TaskService {
     private TaskRepository taskRepository;
 
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
     @Autowired
     public TaskServiceImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
+
 
     @Override
     public List<Task> findAll() {
         return this.taskRepository.findAll();
     }
 
+
     @Override
     public Task findByID(int Id) {
         Optional<Task> result = taskRepository.findById(Id);
-
         Task theTask = null;
 
         if (result.isPresent()) {
@@ -43,34 +37,20 @@ public class TaskServiceImpl implements TaskService {
         } else {
             throw new RuntimeException("NO TASK of ID " + Id);
         }
-
         return theTask;
-
-
     }
-
-
 
 
     @Override
     public Task save(Task theTask) {
-
         return taskRepository.save(theTask);
     }
 
+
     @Override
-//    @Transactional
+    @Transactional
     public void deleteById(int Id) {
         taskRepository.deleteById(Id);
-//        entityManager.joinTransaction();
-        int query = entityManager.createNativeQuery("SELECT setval(pg_get_serial_sequence('tasks', 'id'), COALESCE(MAX(id), 0)) FROM tasks").executeUpdate();
+        taskRepository.resetTaskIdSequence();
     }
-
-//    @Transactional
-//    protected void resetSequence() {
-//        // Set the sequence to continue from the last used ID
-//        entityManager.joinTransaction();
-//        entityManager.createNativeQuery("SELECT setval(pg_get_serial_sequence('tasks', 'id'), COALESCE(MAX(id), 0)) FROM tasks")
-//                .executeUpdate();
-//    }
 }
